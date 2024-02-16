6eebc5ccccac42b807751183b2000efd58293df3
Jjenny-K <kkjjh0129@gmail.com>
2024-02-16 19:57:12 +0900

    Add: Initial settings

---
+++ src/main/java/com/preorderpurchase/PreorderpurchaseApplication.java
@@ -0,0 +1,13 @@
+package com.preorderpurchase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreorderpurchaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreorderpurchaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/preorderpurchase/PreorderpurchaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.preorderpurchase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreorderpurchaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
