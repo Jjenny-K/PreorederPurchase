c620c9a53c8d3a0f50e8d363c0ccb80fe008eb04
Jjenny-K <kkjjh0129@gmail.com>
2024-02-26 18:36:26 +0900

    Fix: Redis settings 수정

--- module_stock_service/src/main/java/com/stock_service/config/RedisConfig.java
+++ module_stock_service/src/main/java/com/stock_service/config/RedisConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
@@ -24,8 +26,23 @@ public class RedisConfig {
     @Bean
     public RedisTemplate<?,?> redisTemplate() {
         RedisTemplate<?,?> redisTemplate = new RedisTemplate<>();
+
         redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+
         return redisTemplate;
+    }
+
+    @Bean
+    public StringRedisTemplate stringRedisTemplate(){
+        StringRedisTemplate stringRedisTemplate=new StringRedisTemplate();
+
+        stringRedisTemplate.setKeySerializer(new StringRedisSerializer());
+        stringRedisTemplate.setValueSerializer(new StringRedisSerializer());
+        stringRedisTemplate.setConnectionFactory(redisConnectionFactory());
+
+        return stringRedisTemplate;
     }
 
 }
