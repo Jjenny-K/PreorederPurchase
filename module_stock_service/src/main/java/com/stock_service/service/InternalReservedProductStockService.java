c4b956f67e1ff87ca4c3defd1e6cd4ed3eecd967
Jjenny-K <kkjjh0129@gmail.com>
2024-02-26 22:19:01 +0900

    Fix: 상품 재고 업데이트 로직 수정-redis 재고 조정

--- module_stock_service/src/main/java/com/stock_service/service/InternalReservedProductStockService.java
+++ module_stock_service/src/main/java/com/stock_service/service/InternalReservedProductStockService.java
@@ -6,6 +6,8 @@ import com.stock_service.reposotiory.ReservedProductStockRepository;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,6 +18,7 @@ public class InternalReservedProductStockService {
     private static final Logger logger = LoggerFactory.getLogger(InternalReservedProductStockService.class);
 
     private final ReservedProductStockRepository reservedProductStockRepository;
+    private final StringRedisTemplate redisTemplate;
 
     // 예약 상품 재고 등록
     @Transactional
@@ -45,7 +48,7 @@ public class InternalReservedProductStockService {
                 reservedProductStockRepository.findByProductId(productId)
                         .orElseThrow(() -> new RuntimeException("해당 상품의 재고가 존재하지 않습니다."));
 
-        if (reservedProductStock.getStock() <= 0) {
+        if (reservedProductStock.getStock() == 0) {
             throw new RuntimeException("해당 상품의 재고가 존재하지 않습니다.");
         }
 
@@ -53,9 +56,20 @@ public class InternalReservedProductStockService {
             throw new RuntimeException("해당 상품의 재고가 부족합니다.");
         }
 
-        Integer updatedStock = reservedProductStock.getStock() - quantity;
+//        Integer updatedStock = reservedProductStock.getStock() - quantity;
+//
+//        reservedProductStock.updateStock(updatedStock);
 
-        reservedProductStock.updateStock(updatedStock);
+        // 예약 상품 재고 감소 + redis
+        String strProductId = String.valueOf(productId);
+        String strStock = String.valueOf(reservedProductStock.getStock());
+
+        ValueOperations<String, String> stockOperations = redisTemplate.opsForValue();
+        stockOperations.set(strProductId, strStock);
+
+        Integer updatedStock = redisTemplate.opsForValue().decrement(strProductId, quantity).intValue();
+
+        reservedProductStock.updateStock(updatedStock <= 0 ? 0 : updatedStock);
     }
 
 }
