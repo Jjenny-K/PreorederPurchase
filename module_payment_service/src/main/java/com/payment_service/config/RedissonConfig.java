e90e9f80513a735bdf654399429e3e37d15137b5
Jjenny-K <kkjjh0129@gmail.com>
2024-02-27 02:29:23 +0900

    Feat: Redis 분산 락 사용을 위한 Redisson settings

---
+++ module_payment_service/src/main/java/com/payment_service/config/RedissonConfig.java
@@ -0,0 +1,38 @@
+package com.payment_service.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.redisson.spring.data.connection.RedissonConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+
+@Configuration
+public class RedissonConfig {
+
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private int port;
+
+    @Value("${spring.redis.host-prefix}")
+    private String HOST_PREFIX;
+
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer()
+                .setAddress(HOST_PREFIX + host + ":" + port);
+
+        return Redisson.create(config);
+    }
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory(RedissonClient redissonClient) {
+        return new RedissonConnectionFactory(redissonClient);
+    }
+
+}
